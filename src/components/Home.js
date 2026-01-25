bc4598fd149fa900bbb1eef7fc85ffebd84d4f00
Wonsun Song <rnsjtm0811@gmail.com>
2026-01-25 22:04:56 +0900

    design

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,9 +10,9 @@ const Home = () => {
           <h1>Wonsun's Portfolio</h1>
           {/* <p>창의적이고 효율적인 솔루션을 만들어나가고 있습니다.</p> */}
           <div className="home-buttons">
-           <a href="#projects" className="btn btn-projects">프로젝트</a>
-           <a href="#Education" className="btn btn-education">학력/경력/교육/자격</a>
-            <a href="#skills" className="btn btn-skills">기술</a>
+           <a href="#projects" className="btn btn-secondary">프로젝트</a>
+           <a href="#Education" className="btn btn-secondary">학력/경력/교육/자격</a>
+            <a href="#skills" className="btn btn-secondary">기술</a>
             <a href="#contact" className="btn btn-secondary">연락처</a>
           </div>
         </div>
